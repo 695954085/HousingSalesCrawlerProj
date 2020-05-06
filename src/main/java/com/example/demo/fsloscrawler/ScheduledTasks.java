ca1cc3d4bddd016e9624ac220070e0be764fd1bc
695954085 <695954085@qq.com>
2020-05-06 23:28:42 +0800

    feat: add scheduled task

---
+++ src/main/java/com/example/demo/fsloscrawler/ScheduledTasks.java
@@ -0,0 +1,25 @@
+package com.example.demo.fsloscrawler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import us.codecraft.webmagic.Spider;
+
+@Component
+public class ScheduledTasks {
+
+    @Autowired
+    private FslosPageProcessor fslosPageProcessor;
+
+    @Autowired
+    private FilePipeline filePipeline;
+
+    @Value("${crawlerUrl}")
+    private String crawlerUrl;
+
+    @Scheduled(cron = "0 0 9 * * *")
+    public void crawler() {
+        Spider.create(fslosPageProcessor).addPipeline(filePipeline).addUrl(crawlerUrl).thread(5).run();
+    }
+}
